0ac67c7d1dd241f94d7ea66a808c3e100abba35d
hyt <1749906634@qq.com>
2023-08-17 16:33:52 +0800

    second commit   2023/8/17   add recycleview details

---
+++ app/src/main/java/com/example/listrecycleview/RecycleDetails.java
@@ -0,0 +1,14 @@
+package com.example.listrecycleview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecycleDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycle_details);
+    }
+}
--- app/src/main/java/com/example/listrecycleview/adapter/RecycleAdapter.java
+++ app/src/main/java/com/example/listrecycleview/adapter/RecycleAdapter.java
@@ -1,9 +1,11 @@
 package com.example.listrecycleview.adapter;
 
 import android.content.Context;
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -23,12 +25,14 @@ public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.RecycleV
 
     @Override
     public RecycleAdapter.RecycleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new RecycleViewHolder(mLayoutInflater.inflate(R.layout.layout_recycle_item,parent,false));
+        return new RecycleViewHolder(mLayoutInflater.inflate(R.layout.layout_recycle_item, parent, false));
     }
 
     @Override
     public void onBindViewHolder(RecycleAdapter.RecycleViewHolder holder, int position) {
-        holder.textView.setText("Hello World!");
+        holder.textView.setText("你好，安卓!");
+        holder.textView2.setText("子线程创建 message 对象，但通过 handle 的 sendMessage（）方法，把它发送到了主线程");
+        holder.imageView.setImageResource(R.drawable.android);
     }
 
     @Override
@@ -40,9 +44,16 @@ public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.RecycleV
 
         private TextView textView;
 
+        private TextView textView2;
+
+        private ImageView imageView;
+
+
         public RecycleViewHolder(@NonNull View itemView) {
             super(itemView);
             textView = itemView.findViewById(R.id.tv1);
+            textView2 = itemView.findViewById(R.id.tv2);
+            imageView = itemView.findViewById(R.id.img_1);
         }
     }
 }
